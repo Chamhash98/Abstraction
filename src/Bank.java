2e6b1c2bc5d145e645ea9a08a2ac8bb38e487c32
Chamhash98 <hashiath869@gmail.com>
2023-11-07 00:02:12 +0530

    abstraction  commit

---
+++ src/Bank.java
@@ -0,0 +1,25 @@
+ abstract class Bank {
+    abstract int interestRate();
+}
+
+class BOC extends Bank{
+    int interestRate(){
+        return 8;
+    }
+}
+
+class HNB extends Bank{
+    int interestRate(){
+        return 10;
+    }
+}
+
+class Test1{
+    public static void main(String[] args) {
+        Bank b;
+        b = new BOC();
+        System.out.println("BOC Rate of Interest : " + b.interestRate() + "%");
+        b = new HNB();
+        System.out.println("HNB Rate of Interest : " + b.interestRate() + "%");
+    }
+}
